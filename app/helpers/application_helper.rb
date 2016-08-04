4cc9d3036bddaed9a7400ad625092cc2a60b5347
Christopher Miller <admin@mctherealm.net>
2016-08-04 14:52:03 -0400

    Added Sign Up Sheet Index Page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def current_user
     session[:user_id] && User.find_by_id(session[:user_id])
   end
+
+  def format_date(date)
+    date.to_formatted_s(:long)
+  end
 end
