7b29a4a2de0b11ee236018086e3c821b88df6e51
Christopher Miller <admin@mctherealm.net>
2016-08-19 08:28:14 -0400

    Added announcement views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,13 @@ module ApplicationHelper
   def format_date(date)
     date.to_formatted_s(:long)
   end
+
+  def format_time(time)
+    time.strftime('%b %d, %Y %I:%M %p')
+  end
+
+  def rank_title(rank)
+    titles = ["Guest", "Member", "Seargent-At-Arms", "Reporter", "Treasurer", "Secretary", "Vice President", "President", "Advisor"]
+    titles[rank]
+  end
 end
