45895529ba3d25e82dcc252f38f2c8123c088b4f
Christopher Miller <admin@mctherealm.net>
2016-12-09 13:13:42 -0500

    Updated events factory

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :event do
-    name "Test Event"
-    description "Hello World!"
+    name 'Test Event'
+    description 'Hello World!'
     max_members_per_team 6
   end
 end
