a0ce54e9123ee40df0a668150f95db74bcc7aecb
Christopher Miller <admin@mctherealm.net>
2016-12-28 11:53:06 -0500

    Added attendance search by date

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -17,6 +17,11 @@ class AdminController < ApplicationController
   end
 
   def attendance
+    if params[:commit] == 'Search'
+      @sign_up_count = SignUpSheet.where(created_at: params[:search][:from].to_date..params[:search][:to].to_date).size
+    else
+      @sign_up_count = SignUpSheet.all.count
+    end
     @users = User.all
     add_breadcrumb 'Attendance Report', :admin_attendance_path
     respond_with @users
